59003d34725a14a63203f059317e98c1733483d9
franckdombou <franckbertrand661@gmail.com>
2025-06-02 04:39:27 +0100

    premier commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,80 @@
-import { StyleSheet } from 'react-native';
+import { View } from 'react-native';
+import React, { useEffect, useMemo, useState } from 'react';
+import ListingsBottomSheet from '@/components/ListingsBottomSheet';
+import listingsData from '@/assets/data/airbnb-listings.json';
+import ListingsMap from '@/components/ListingsMap';
+import listingsDataGeo from '@/assets/data/airbnb-listings.geo.json';
+import { Stack } from 'expo-router';
+import ExploreHeader from '@/components/ExploreHeader';
+import { doc, getDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+//import { db } from './firebase'; // ton fichier firebase config
+import { db} from "../../firebaseConfig"
+//import { useEffect, useState } from "react";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+interface Product {
+  id: string;
+  nom: string;
+  position: [number, number];
+  statut: string;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+
+
+
+const Page = () => {
+  const items = useMemo(() => listingsData as any, []);
+  const getoItems = useMemo(() => listingsDataGeo, []);
+  const [category, setCategory] = useState<string>('Tiny homes');
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "products"));
+        const productsList: Product[] = querySnapshot.docs.map((doc, index) => {
+          const data = doc.data() as {
+            Titre?: string;
+            Latitude: string;
+            Longitude: string;
+            Statut?: string;
+          };
+
+          return {
+            id: doc.id,
+            nom: data.Titre || `Maison ${index + 1}`,
+            position: [parseFloat(data.Latitude), parseFloat(data.Longitude)],
+            statut: data.Statut || "Fin",
+          };
+        });
+
+        setProducts(productsList);
+        console.log("Maisons formatées :", productsList);
+      } catch (error) {
+        console.error("Erreur lors du chargement des produits :", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);  
+
+  const onDataChanged = (category: string) => {
+    setCategory(category);
+  };
+
+  return (
+    <View style={{ flex: 1, marginTop: 80 }}>
+      {/* Define pour custom header */}
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+      <ListingsMap listings={getoItems} />
+      <ListingsBottomSheet listings={items} category={category} />
+    </View>
+  );
+};
+
+export default Page;
